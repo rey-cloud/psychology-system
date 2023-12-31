61000fd68cf75af8d33005b322bec9465583a912
Reynaldo A. Baja Jr <reynaldobaja2@gmail.com>
2023-12-31 12:31:37 +0800

    baja-update(change name)

--- client/app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <div class=" flex text-center ">
-      <p class="m-auto bg-slate-500 w-full text-2xl h-screen">Hello World</p>
-    </div>
-  </div>
-</template>
---
+++ nuxt/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="flex text-center">
+      <p class="m-auto bg-slate-500 w-full text-2xl h-screen">
+        Hello World Iyot
+      </p>
+    </div>
+  </div>
+</template>
