96c021ccc4a03d1f2c9e671671693d004dce2dd1
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2024-01-03 09:55:26 +0800

    kent-update(deleted app.vue)

--- nuxt/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div class="flex text-center">
-      <p class="m-auto bg-slate-500 w-full text-2xl h-screen">
-        Hello World I love You Kent Escoto -L
-      </p>
-    </div>
-  </div>
-</template>
